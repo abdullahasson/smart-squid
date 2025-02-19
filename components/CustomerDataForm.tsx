4273cbb676f76287150ee890459350974d1b5ef3
abdullahasson <bh532997@gmail.com>
2025-02-20 00:26:35 +0200

    add Input component

--- components/CustomerDataForm.tsx
+++ components/CustomerDataForm.tsx
@@ -1,16 +1,31 @@
+// Input 
+import { Input } from "@/components/ui/input"
+
 const CustomerDataForm = () => {
   return (
     <div className="flex flex-col">
-        <h2 className="text-2xl font-bold">البيانات العميل</h2>
-        <form action="">
-            <input type="text" />
-            <input type="email" />
-            <input type="tel" />
-            <input type="text" />
-            <textarea name="" id=""></textarea>
-            <textarea name="" id=""></textarea>
-            <button type="submit">تأكيد</button>
-        </form>
+      <h2 className="text-2xl font-bold">البيانات العميل</h2>
+      <form action="" className="flex flex-col gap-4">
+        <div>
+          <Input type="text" />
+        </div>
+        <div>
+          <Input type="email" />
+        </div>
+        <div>
+          <Input type="tel" />
+        </div>
+        <div>
+          <Input type="text" />
+        </div>
+        <div>
+          <textarea name="" id=""></textarea>
+        </div>
+        <div>
+          <textarea name="" id=""></textarea>
+        </div>
+        <button type="submit">تأكيد</button>
+      </form>
     </div>
   )
 }

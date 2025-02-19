7a9c0fe967388ca5d25691e4c84d58882598ffb3
abdullahasson <bh532997@gmail.com>
2025-02-19 23:15:21 +0200

    add calendar

--- app/book-meeting/page.tsx
+++ app/book-meeting/page.tsx
@@ -1,27 +1,27 @@
-// "use client"
+"use client"
 
-// import { useState } from "react"
+import { useState } from "react"
 
-// import { Calendar } from "@/components/ui/calendar"
+import { Calendar } from "@/components/ui/calendar"
 import CustomerDataForm from "@/components/CustomerDataForm";
 
-const page = () => {
-  // const [date, setDate] = useState<Date | undefined>(new Date())
+const BookMeeting = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date())
 
   return (
     <div>
       <div className="container mx-auto flex justify-between">
         <CustomerDataForm />
 
-        {/* <Calendar
+        <Calendar
           mode="single"
           selected={date}
           onSelect={setDate}
           className="border-none"
-        /> */}
+        />
       </div>
     </div>
   )
 }
 
-export default page
+export default BookMeeting

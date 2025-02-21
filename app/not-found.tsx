3c6df761ed649851361959d99616dcf65484aba6
abdullahasson <bh532997@gmail.com>
2025-02-21 22:32:25 +0200

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+    return (
+        <div className="not-found flex-[4] p-9 flex flex-col gap-12">
+            <h1 className="text-center text-2xl font-semibold poppins">
+                هذه الصفحة غير موجودة في ملف الفيغما بعد
+            </h1>
+        </div>
+    )
+}
+
+export default NotFoundPage;
